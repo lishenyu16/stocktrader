5d64b2f57d50f15905eec2e9f6b94311fb6c195e
Shenyu <lishenyu16@gmail.com>
2018-09-28 21:32:00 +0800

    design stocks/stock panel

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>The Stock-Stocks component</h1>
+    <div>
+        <app-stock v-for="stock in stocks">
+
+        </app-stock>
+    </div>
 </template>
+<script>
+    import Stock from './Stock.vue'
+    export default {
+        data() {
+            return {
+                stocks: [
+                    { id:1,name:"BMW",price:110 },
+                    { id:2,name:"APPLE",price:320},
+                    { id:3,name:"GOOGL",price:400 },
+                    { id:4,name:"AMAZON",price:421 }
+                ]
+            }
+        },
+        components:{
+            appStock:Stock
+        }
+    }
+</script>
